c39bee05572968bd49e85104b26b8da6600893b9
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-07-10 23:37:00 +0500

    feat: sidenav overlay

---
+++ side navigation/sidenav overlay/src/main.ts
@@ -0,0 +1,14 @@
+const openNavButton = document.querySelector('.open_nav') as HTMLButtonElement;
+const closeBtn = document.getElementById("close") as HTMLElement;
+const navigation_left = document.querySelector(".navigation__left") as HTMLElement;
+const navigation_right = document.querySelector('.navigation__right') as HTMLDivElement;
+
+openNavButton.addEventListener("click" , () => {
+    navigation_right.classList.remove("position_index");
+    navigation_left.classList.remove('width');
+});
+
+closeBtn.addEventListener("click" , () => {
+    navigation_right.classList.add("position_index");
+    navigation_left.classList.add('width');
+})
